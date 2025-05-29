13a12d1894838a211a10dbcba898f7d8de787876
anandadr5 <anandadwir898@gmail.com>
2025-05-29 19:57:38 +0700

    fix: final deployment config for Railway

--- app.py
+++ app.py
@@ -295,6 +295,6 @@ init_db()
 
 # RUN APP
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', '8080'))
+    port = int(os.environ.get('PORT', 8080))
     debug_mode = os.environ.get('FLASK_ENV') == 'development'
     app.run(host='0.0.0.0', port=port, debug=debug_mode)
